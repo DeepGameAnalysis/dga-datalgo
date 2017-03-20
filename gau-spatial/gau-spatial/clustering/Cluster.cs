216d19ce6cae5c9acf5bb624a00173c704607fcb
Patrick Matthäi <pmd.matthaei@gmail.com>
2017-03-20 20:31:41 +0100

    Clustering package initial.

---
+++ gau-spatial/gau-spatial/clustering/Cluster.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSGO_Analytics.src.math;
+using System.Collections;
+
+/// <summary>
+/// TODO: make generic some time
+/// </summary>
+namespace Clustering
+{
+    public class Cluster
+    {
+        public List<EDVector3D> data;
+
+        public EDVector3D centroid;
+
+
+        public Cluster()
+        {
+            this.data = new List<EDVector3D>();
+
+        }
+
+        public Cluster(EDVector3D[] data)
+        {
+            this.data = data.ToList();
+        }
+
+        public Cluster(EDVector3D datapoint)
+        {
+            this.data = new List<EDVector3D>();
+            assignToCluster(datapoint);
+        }
+
+
+        internal void assignToCluster(EDVector3D p)
+        {
+            data.Add(p);
+        }
+
+        public EDRect getBoundings()
+        {
+            var min_x = data.Min(point => point.X);
+            var min_y = data.Min(point => point.Y);
+            var max_x = data.Max(point => point.X);
+            var max_y = data.Max(point => point.Y);
+            var dx = max_x - min_x;
+            var dy = max_y - min_y;
+            return new EDRect { X = min_x, Y = max_y, Width = dx, Height = dy };
+        }
+
+
+        public void AddPosition(EDVector3D p)
+        {
+            data.Add(p);
+        }
+    }
+}
---
+++ gau-spatial/gau-spatial/clustering/LEADERClustering.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clustering
+{
+    class LEADERClustering
+    {
+        public float delta { get; set; }
+
+        private List<Cluster> clusters = new List<Cluster>();
+
+        private List<EDVector3D> leaders = new List<EDVector3D>();
+
+        public LEADERClustering(List<EDVector3D> pos)
+        {
+            //var sorted_pos = pos.OrderBy(p => p.X).ThenBy(p => p.Y).ToList();
+            var sorted_pos = pos.OrderBy(p => p.Y).ThenBy(p => p.X).ToList();
+            clusterData(sorted_pos);
+        }
+
+        public LEADERClustering(float delta)
+        {
+            this.delta = delta;
+        }
+
+        /// <summary>
+        /// Probleme: Reihenfolge der Punkte
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <returns></returns>
+        public Cluster[] clusterData(List<EDVector3D> pos)
+        {
+            clusters.Clear();
+            Cluster start_cluster = new Cluster(pos[0]);
+            int leader_ind = 0;
+            leaders.Add(pos.First());
+
+            clusters.Add(start_cluster);
+
+            for (int i = 1; i < pos.Count; i++)
+            {
+                var datapoint = pos[i];
+
+                var min_dist = 0.0;
+                var min_dist_leader_index = 0;
+                for (int index = 0; index < leaders.Count; index++)
+                {
+                    var leader_distance = EDMathLibrary.getEuclidDistance2D(leaders[index], datapoint);
+                    if (index == 0) {
+                        min_dist = leader_distance;
+                        continue;
+                    }
+                    if (leader_distance < min_dist)
+                    {
+                        min_dist = leader_distance;
+                        min_dist_leader_index = index;
+                    }
+                }
+
+                if (min_dist < delta)
+                {
+                    clusters[min_dist_leader_index].AddPosition(datapoint);
+                }
+                else
+                {
+                    Cluster new_cluster = new Cluster(datapoint);
+                    clusters.Add(new_cluster);
+                    leader_ind++;
+                    leaders.Add(datapoint);
+                }
+            }
+            leaders.Clear();
+            return clusters.ToArray();
+        }
+
+    }
+}
